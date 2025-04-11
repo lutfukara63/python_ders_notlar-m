e051d3b34b096e1ff4082c9ee0e40e385f7100b0
Lütfü Karakeçili <o2301050027@ogr.igdir.edu.tr>
2025-04-11 22:22:31 +0300

    ne olduğu anlatık

---
+++ konu1.py
@@ -0,0 +1,34 @@
+"""
+# tek satırlık yorum için
+"""  """
+''' '''
+3 tek veya çift tırnak birden fazla yorum  satırı için kullanılır
+
+int = integer sayı için
+float = ondalıklı sayı için
+str = string metin için 
+bool = boolean doğru yanlış için
+
+
+== = eşit mi
+!= = eşit değil mi
+<= = küçük eşit mi
+
+üs alma ** 2**3 = 8
+mod alma % 5%2 = 1
+# // tam sayı bölme 5//2 = 2
+
+a=5
+a+=3
+a=8
+
+maaş+(maaş zammı)
+maaş+=maaş*zammı
+aynı değildir çünkü maaş zammı maaşın %'i kadar artar
+
+inddirim  geçiyoröu = koşular
+print(indirim geçiyor mu?)
+indirim geçiyor mu = True veya False olur
+
+
+"""
